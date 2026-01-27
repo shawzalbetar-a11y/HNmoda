b378cfdb57ea2e1d6935b87e76c2970ec14a9d20
huseyin X8 <shawzalbetar@gmail.com>
2026-01-27 21:25:01 +0000

    لم يظهر بعد اللوجو  هل ارفع لك لوجو بصيغة png

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -35,7 +35,7 @@ export function Header() {
               style={{
                 width: '48px',
                 height: '48px',
-                backgroundImage: "url('/logo0.svg')",
+                backgroundImage: "url('/logo.png')",
                 backgroundSize: 'contain',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'center',
