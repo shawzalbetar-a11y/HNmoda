88d338c43bb3f529ca37f00edd430d29948246f4
hnmoda2-ui <hnmoda2@gmail.com>
2026-02-01 00:49:05 +0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
         whatsappNumber: '905449996865',
         phone: '+90 544 999 68 65',
         email: 'info@hnmoda.tr',
-        telegram: '@HUMAN_NATURE_HN',
+        telegram: '@HN_MODA',
         address: 'Istanbul, Turkey',
     }
 }
